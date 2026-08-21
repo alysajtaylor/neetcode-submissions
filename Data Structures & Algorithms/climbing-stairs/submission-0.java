a1041fab4001bdddd7cc88df9e76027cbddbd05d
Jennifer Taylor <alysajtaylor@outlook.com>
2026-08-21 12:47:46 -0700

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        return climbStairs(n, new HashMap<>());
+    }
+    public int climbStairs(int n, HashMap<Integer,Integer> memo) {
+        if(n <= 2) return n;
+        if(memo.containsKey(n)) return memo.get(n);
+        
+        int result = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
+        memo.put(n, result);
+        return result;
+    }
+}
